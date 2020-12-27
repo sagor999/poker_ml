3137fe38679f014537d5c4285dac0b6b0ad4b2c9
Pavel Tumik <pavel.tumik@gmail.com>
2020-12-27 11:17:28 -0800

    add OCR support

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,4 +1,20 @@
 from PIL import Image
 import pytesseract
 
-print(pytesseract.image_to_string(Image.open('/data/ocr5.png')))
+import sys
+import pathlib
+import time
+
+
+#print(pytesseract.image_to_string(Image.open('/data/ocr6.png')))
+
+while True:
+  trigger_file = pathlib.Path("/data/trigger")
+  if trigger_file.is_file():
+    trigger_file.unlink()
+    output = pytesseract.image_to_string(Image.open('/data/ocr.png'), timeout=3, lang='eng')
+    [print(line) for line in output.split('\n') if line.strip() != '']
+    print('END')
+    sys.stdout.flush()
+  else:
+    time.sleep(0.1)
