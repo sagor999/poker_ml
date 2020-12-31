68b2c155bba53fbf81f6ac20306ac212af930c92
Pavel Tumik <pavel.tumik@gmail.com>
2020-12-31 09:53:24 -0800

    re-enable ocr. fix unicode bug. add ocr for call action.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run:
 	-docker stop $$(docker ps -aq)
 	cd card_recognizer_ml; make run
-	#cd ocr; make run
+	cd ocr; make run
 	python3 orchestrator/main.py
--- ocr/ocr.py
+++ ocr/ocr.py
@@ -4,7 +4,7 @@ import pytesseract
 import sys
 import pathlib
 import time
-
+from unidecode import unidecode
 
 #print(pytesseract.image_to_string(Image.open('/data/ocr6.png')))
 
@@ -12,9 +12,16 @@ while True:
   trigger_file = pathlib.Path("/data/trigger")
   if trigger_file.is_file():
     trigger_file.unlink()
-    output = pytesseract.image_to_string(Image.open('/data/ocr.png'), timeout=3, lang='eng')
+    output = unidecode(pytesseract.image_to_string(Image.open('/data/ocr.png'), timeout=1, lang='eng'))
     [print(line) for line in output.split('\n') if line.strip() != '']
     print('END')
     sys.stdout.flush()
-  else:
-    time.sleep(0.1)
+  trigger_file = pathlib.Path("/data/trigger2")
+  if trigger_file.is_file():
+    trigger_file.unlink()
+    output = unidecode(pytesseract.image_to_string(Image.open('/data/ocr2.png'), timeout=1, lang='eng'))
+    [print(line) for line in output.split('\n') if line.strip() != '']
+    print('END')
+    sys.stdout.flush()
+
+  time.sleep(0.1)
